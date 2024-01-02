eac4b6a580f3a208a64b3e80858ac7ac4959ab20
Ganeshpandhare <151309455+Ganeshpandhare@users.noreply.github.com>
2024-01-02 22:27:50 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-tf-test-s3-bucket-ganesh2"
+    bucket = "my-tf-test-s3-bucket-ganesh1"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
